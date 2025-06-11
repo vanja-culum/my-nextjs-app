573c9d57eeded00cb03284014bb5218adf98cce3
vanja-culum <vanja.culum@fatcatcoders.com>
2025-06-11 20:06:10 +0200

    refactor, add bst logic

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Loader2Icon } from "lucide-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -39,20 +40,33 @@ function Button({
   className,
   variant,
   size,
+  loading = false,
+  loadingText = 'Loading...',
   asChild = false,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
+    loading?: boolean
+    loadingText?: string
   }) {
   const Comp = asChild ? Slot : "button"
-
+  const { children, ...restProps } = props;
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
+      disabled={loading || props.disabled}
+      {...restProps}
+    >
+      {
+        loading && <>
+          <Loader2Icon className="animate-spin" />
+          {loadingText}
+        </>
+      }
+      {!loading && children}
+    </Comp>
   )
 }
 
--- src/sections/DSACode/components/BstVisualizer.tsx
+++ src/sections/DSACode/components/BstVisualizer.tsx
@@ -12,10 +12,16 @@ import ReactFlow, {
   type Node,
   type OnConnect,
   type Edge,
+  ConnectionMode,
 } from 'reactflow';
+import { Input } from '@/components/ui/input';
 
 import 'reactflow/dist/style.css';
-import { BstGenerateDialog } from './BstGenerateDialog';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { env } from '@/env';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const initialNodes: Node[] = [
   { id: '1', draggable: false, position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -24,29 +30,73 @@ const initialNodes: Node[] = [
 ];
 
 const initialEdges: Edge[] = [
-  { 
-      id: 'e1-2', 
-      source: '1', 
-      target: '2' 
-  }, 
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2'
+  },
   {
     id: 'e1-3',
     source: '1',
     target: '3',
-}];
+  }];
 
+const bstTypes = [
+  { value: 'random', label: 'Random' },
+  { value: 'balanced', label: 'Balanced' },
+  { value: 'unbalanced', label: 'Unbalanced' },
+  { value: 'perfect', label: 'Perfectly balanced' },
+]
 
 export const BstVisualizer = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+  const form = useForm({
+    defaultValues: {
+      nodeCount: '10',
+      type: 'random'
+    },
+  })
+
+  const { control, handleSubmit, formState: { isSubmitting } } = form
+
+
   const onConnect = useCallback<OnConnect>((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
-  const onGenerate = useCallback(data => {
-    console.log('data', data);
-    setNodes(data.nodes)
-    setEdges(data.edges)
-  })
+
+  async function onSubmit(data: { nodeCount: string }) {
+    const payload = {
+      ...data,
+      nodeCount: +data.nodeCount
+    }
+
+    console.log('onSubmit', data);
+    try {
+      const response = await fetch(env.NEXT_PUBLIC_API_BASE_URL + '/dsa/bst/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+
+      const result = await response.json();
+
+      result.edges
+        .forEach((edge: Edge) => {
+          edge.type = 'straight'
+        });
+
+      console.log('result', result);
+      setNodes(result.nodes)
+      setEdges(result.edges)
+    } catch (error) {
+      console.log('error', error);
+    }
+
+  }
 
   return <div className='container flex-col gap-8 my-16 h-[56rem]'>
     <Heading className='text-center mb-8'>
@@ -61,10 +111,50 @@ export const BstVisualizer = () => {
     </Paragraph>
 
     <div className='flex gap-4 my-4'>
-      <BstGenerateDialog onGenerate={onGenerate} />
+      <Form {...form}  >
+        <form onSubmit={handleSubmit(onSubmit)} className='flex my-4 items-end gap-4'>
+          <FormField control={control} render={({ field }) => (
+            <FormItem>
+              <FormLabel>Node Count</FormLabel>
+              <Input {...field} type='number' placeholder='Number of nodes' />
+            </FormItem>
+          )} name='nodeCount' />
+
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Type</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select BST type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {bstTypes.map((type) => (
+                      <SelectItem key={type.value} value={type.value}>
+                        {type.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+
+          <div>
+            <Button loading={isSubmitting} type="submit">Generate</Button>
+          </div>
+        </form>
+      </Form>
     </div>
     <ReactFlow
       fitView
+      connectionMode={ConnectionMode.Strict}
       className='!h-[80%]'
       draggable={false}
       nodes={nodes}
